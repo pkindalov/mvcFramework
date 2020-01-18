0b3d4af051f9cfbd34e520a2dc5e3083367f8640
pkindalov <panayotkindalov@gmail.com>
2020-01-18 15:03:51 +0200

    Updated files bootstrap, Core.php, index.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,5 +3,3 @@
     require_once 'libraries/Core.php';
     require_once 'libraries/Controller.php';
     require_once 'libraries/Database.php';
-
-   
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,1 +1,22 @@
-Hello
+<?php
+    /*
+     * App core class 
+     * Creates URL & loads core controller 
+     * URL format - /controller/method/params
+     */
+
+     class Core{
+         protected $currentController = 'Pages';
+         protected $currentMethod = 'index';
+         protected $params = [];
+
+         public function __construct()
+         {
+             $this->getURL();
+         }
+
+         public function getURL(){
+             echo $_GET['url'];
+         }
+
+     }
