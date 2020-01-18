f8e7ee8a998f5b6b397c04a3ff07240404cd27f3
pkindalov <panayotkindalov@gmail.com>
2020-01-18 22:42:05 +0200

    Refactored bootstrap.php and updated config.php

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+    //App root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URL root
+    define('URLROOT', 'http://http://localhost/mvcFramework/'); 
+
+    //Site name
+    define('SITENAME', 'mvcFramework');
