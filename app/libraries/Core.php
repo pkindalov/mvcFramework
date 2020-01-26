31f5fdf82864a64bc3c82b8cf4c284473601d661
pkindalov <panayotkindalov@gmail.com>
2020-01-26 15:16:44 +0200

    Little fix in Core file

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -41,7 +41,7 @@
             }
 
             //Get params
-            $this->params = $url ? array_values($url) : [];
+            $this->params = $url ? array_values($url) : ['data' => ''];
 
             //Call a callback with array of params
             call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
